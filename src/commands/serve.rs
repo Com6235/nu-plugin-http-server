a9f63ab248f394141020ee4933c3142389abca58
Com6235 <ru.nms199@gmail.com>
2025-03-20 16:30:07 +0300

    Make `http serve` use pipeline metadata to auto determine mime-type; Update nu dependencies to 0.103.0

--- src/commands/serve.rs
+++ src/commands/serve.rs
@@ -2,11 +2,19 @@ use std::{io::{stdout, Write}, vec};
 
 use nu_plugin::{PluginCommand, EvaluatedCall, EngineInterface};
 use nu_protocol::{ErrorLabel, Example, LabeledError, PipelineData, PipelineMetadata, Signature, SyntaxShape, Type, Value};
-use tokio::sync::oneshot::Sender;
 
 use crate::{router::{server::Server, Route}, HttpServerPlugin};
 
 pub struct HttpServeCommand;
+
+fn parse_pipeline_mime(pipeline: Option<PipelineMetadata>, default: &str) -> String {
+    let mut str = pipeline
+        .unwrap_or(PipelineMetadata::default())
+        .content_type
+        .unwrap_or(String::from(default));
+    str.push_str("; charset=utf-8");
+    str
+}
 
 impl PluginCommand for HttpServeCommand {
     type Plugin = HttpServerPlugin;
@@ -35,7 +43,7 @@ impl PluginCommand for HttpServeCommand {
     }
 
     fn extra_description(&self) -> &str {
-        "Binds a HTTP server to a random (or given) address and port. \nPlease provide a mime-type with --mime (-m) for any non-text data."
+        "Binds a HTTP server to a random (or given) address and port."
     }
 
     fn signature(&self) -> nu_protocol::Signature {
@@ -64,12 +72,11 @@ impl PluginCommand for HttpServeCommand {
         let (data, given_mime): (Vec<u8>, String) = match input {
             PipelineData::Empty => (vec![], String::from("text/plain; charset=utf-8")),
             PipelineData::Value(val, meta) => {
-                eprintln!("{}", meta.unwrap_or(PipelineMetadata::default()).content_type.unwrap_or(String::new()));
                 match val {
-                    Value::String { val, internal_span: _ } => (val.as_bytes().to_vec(), String::from("text/plain; charset=utf-8")),
+                    Value::String { val, internal_span: _ } => (val.as_bytes().to_vec(), parse_pipeline_mime(meta, "text/plain")),
                     Value::Nothing { internal_span: _ } => (vec![], String::from("text/plain")),
-                    Value::Bool { val, internal_span: _ } => ((if val { "true" } else { "false" }).as_bytes().to_vec(), String::from("text/plain; charset=utf-8")),
-                    Value::Binary { val, internal_span: _ } => (val, String::from("application/octet-stream")),
+                    Value::Bool { val, internal_span: _ } => ((if val { "true" } else { "false" }).as_bytes().to_vec(), parse_pipeline_mime(meta, "text/plain")),
+                    Value::Binary { val, internal_span: _ } => (val, parse_pipeline_mime(meta, "application/octet-stream")),
                     _ => {
                         return Err(LabeledError { msg: String::from("Not Implemented! (Value::*)"), labels: Box::new(vec![ErrorLabel { span: val.span(), text: String::from("Unimplemented data type.") }]), code: None, url: None, help: None, inner: Box::new(vec![]) });
                     }
@@ -78,11 +85,11 @@ impl PluginCommand for HttpServeCommand {
             PipelineData::ListStream(_val, _) => {
                 return Err(LabeledError { msg: String::from("Not Implemented! (PipelineData::ListStream)"), labels: Box::new(vec![]), code: None, url: None, help: None, inner: Box::new(vec![]) });
             }
-            PipelineData::ByteStream(val, _) => {
+            PipelineData::ByteStream(val, meta) => {
                 let a = val.into_value().unwrap();
                 match a {
-                    Value::Binary { val, internal_span: _ } => (val, String::from("application/octet-stream")),
-                    Value::String { val, internal_span: _ } => (val.as_bytes().to_vec(), String::from("text/plain; charset=utf-8")),
+                    Value::Binary { val, internal_span: _ } => (val, parse_pipeline_mime(meta, "application/octet-stream")),
+                    Value::String { val, internal_span: _ } => (val.as_bytes().to_vec(), parse_pipeline_mime(meta, "text/plain")),
                     _ => { return Err(LabeledError { msg: String::from("How did you get here?"), labels: Box::new(vec![ErrorLabel { span: a.span(), text: String::from("Literally how?") }]), code: None, url: None, help: None, inner: Box::new(vec![]) }); }
                 }
             }
@@ -94,7 +101,7 @@ impl PluginCommand for HttpServeCommand {
             let (task, local_addr) = Server::new(bind_addr, vec![Route::new(String::from("/"), data, mime)]).start().await;
 
             let (tx, rx) = tokio::sync::oneshot::channel::<bool>();
-            let signal = engine.register_signal_handler(Box::new(move |_| { let _ = Sender::clone(&tx).send(true); })).unwrap();
+            let signal = engine.register_signal_handler(Box::new(move |_| { let _ = tx.clone().send(true); })).unwrap();
 
             if !engine.is_using_stdio() {
                 let _ = stdout().write_all(String::from(format!("Binded to: http://{}:{}/", local_addr.ip().to_string(), local_addr.port())).as_bytes());
